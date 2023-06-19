0f1fdcb5bdeb2ac5922124499fe141bcf61ce002
superwomanliuda <abbyliuwp@gmail.com>
2023-06-19 14:35:39 +0800

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    image: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
